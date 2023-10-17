fcb2ba91ada48178536bcc4a2b4d6f04099166ac
austin <harrenaustint@gmail.com>
2023-10-16 20:33:23 -0400

    push

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+const About = () => {
+  return (
+    <div className='container'>
+        <h4 className="center">About</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore earum delectus dolore enim dignissimos! Dolores expedita neque ut perferendis quis, voluptatibus laboriosam! Nam magnam ab officiis, maiores officia quaerat quibusdam?</p>
+    </div>
+  )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = (props) => {
+// setTimeout(() =>{
+//     props.history.push('/about')
+// }, 2000)
+  return (
+    <div className='container'>
+        <h4 className="center">Contact</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore earum delectus dolore enim dignissimos! Dolores expedita neque ut perferendis quis, voluptatibus laboriosam! Nam magnam ab officiis, maiores officia quaerat quibusdam?</p>
+    </div>
+  )
+}
+
+export default Contact
